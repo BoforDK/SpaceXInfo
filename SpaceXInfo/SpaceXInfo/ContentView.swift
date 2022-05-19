8dc13dfa8d37129bb6fe9985989623518007e4e7
Alexander Grigorov <BoforDK@gmail.com>
2022-05-19 21:56:56 +0200

    init

---
+++ SpaceXInfo/SpaceXInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceXInfo
+//
+//  Created by Alexander Grigorov on 19.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceXInfo/SpaceXInfo/SpaceXInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXInfoApp.swift
+//  SpaceXInfo
+//
+//  Created by Alexander Grigorov on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
